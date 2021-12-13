6fd88751fbde6e2813316e9d77ce4fcbfe7a7bbc
ASITHA INDRAJITH DIVISEKARA <46625806+Divisekara@users.noreply.github.com>
2021-12-13 11:56:10 +0530

    headers

---
+++ classes for seperate files.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+// say we have bunch of classes then we cant put all classes into one file where main code is written.
+// therefore we put seperate classes into seperate files.
+// if you are working with couple of people at a time then its easy to work single file or one file for one person
+// we uncheck has destructor and virtual destructor
+// usually all the class files locally on the same directory where main cpp file is located
+
+// we need to make 2 files in order to create a header file
+// first one is a header file where all the classes, function prototyping and variables are stored
+// we use the extension .h for the header file
+// in cpp source file where the extension is .cpp that is the file where we actually creating the objects and building the functions. 

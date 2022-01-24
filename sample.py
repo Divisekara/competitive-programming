10d141cb07567645ac128c94af90569ee4a8d0aa
ASITHA INDRAJITH DIVISEKARA <46625806+Divisekara@users.noreply.github.com>
2022-01-24 19:54:20 +0530

    time consumption for an python algorithm

--- sample.py
+++ sample.py
@@ -1,17 +1,21 @@
 import time
-from tracemalloc import start
 
 from pyparsing import restOfLine
 
 start_time = time.time()
 
 result = 0
-for i in range(0,100000):
+for i in range(-1000000,1000000):
+    if(i%2==0):
+        pass
     result += i
 
 print(result)
 
-time.sleep(1)
-end_time = time.time()
 
+end_time = time.time()
 print("time consumed = {0:.5f}ms ".format((end_time-start_time)*1000))
+
+
+
+# print("{0:.5f}".format(123456789.123456789))

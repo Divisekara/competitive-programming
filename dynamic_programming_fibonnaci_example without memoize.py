b43efd185fb5641f47c9ec2c6fd39346fed9fae1
ASITHA INDRAJITH DIVISEKARA <46625806+Divisekara@users.noreply.github.com>
2022-01-24 20:40:55 +0530

    usage of memoizing

---
+++ dynamic_programming_fibonnaci_example without memoize.py
@@ -0,0 +1,32 @@
+from threading import Timer
+
+import time
+start_time = time.time()
+
+def fib(n):
+    if(n<2):
+        f = 1
+    else:
+        f = fib(n-1) + fib(n-2)
+    return f
+
+term = fib(35)
+print(term)
+
+end_time = time.time()
+
+print("{:.10f}ms".format((end_time-start_time)*1000))
+
+
+
+# we can see that without using the memoize the algorithm time complexity exponentially grwos which is two to the power of n = 2^n
+# the time gets double almost everytime we increase the number by 1.
+# for the 40th term it takes more than 20seconds to calculate the fibbonacci
+
+
+
+
+
+
+
+
